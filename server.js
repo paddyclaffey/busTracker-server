d14749d19ad24dda30a6b3736ffd4bdb84f86208
paddyclaffey <paddyclaffey@gmail.com>
2019-01-27 15:38:26 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+var quotes = [
+  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
+  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
+  { author : 'Unknown', text : "Even the greatest was once a beginner. Don’t be afraid to take that first step."},
+  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you’re afraid to live. What a way to exist."}
+];
+
+var location = null;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/locationUpdate', function(req, res) {
+    location = req.body;
+    console.log(location);
+
+    res.send({ sucess: 'Location Saved' });
+});
+
+app.get('/location', function(req, res) {
+  res.json(location);
+});
+
+app.listen(process.env.PORT || 3412);
